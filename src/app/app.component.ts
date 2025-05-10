6fd9a1fb8e69698e3162e28c14bac198e3bc307d
Henry Reaño <henry29.2004@hotmail.com>
2025-05-10 15:03:15 -0500

    feat(footer): add footer content component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
-import {TranslateService, TranslatePipe} from '@ngx-translate/core';
+import {TranslateService} from '@ngx-translate/core';
 import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
+import {FooterContentComponent} from './public/components/footer-content/footer-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    TranslatePipe,
-    LanguageSwitcherComponent
+    LanguageSwitcherComponent,
+    FooterContentComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

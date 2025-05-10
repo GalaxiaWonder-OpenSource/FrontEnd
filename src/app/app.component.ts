c2cb80c3fb47b25af36b628a8e92910e7738cc8e
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-05-10 12:17:34 -0500

    feat(i18n): add language switcher component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {TranslateService, TranslatePipe} from '@ngx-translate/core';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    LanguageSwitcherComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

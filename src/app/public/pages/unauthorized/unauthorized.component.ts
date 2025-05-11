d9f54280a1c3efa2cf2bace8e8b4072aa7445ecf
Henry Reaño <henry29.2004@hotmail.com>
2025-05-10 23:38:56 -0500

    chore: add unauthorized page routing

---
+++ src/app/public/pages/unauthorized/unauthorized.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  imports: [],
+  standalone: true,
+  templateUrl: './unauthorized.component.html',
+  styleUrl: './unauthorized.component.css'
+})
+export class UnauthorizedComponent {
+
+}

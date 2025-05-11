2f0eefc4df99d01522b756f7b1b75785efac83b1
Henry Reaño <henry29.2004@hotmail.com>
2025-05-10 23:46:26 -0500

    chore: add application route architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,24 @@ import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
 import {FooterContentComponent} from './public/components/footer-content/footer-content.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     LanguageSwitcherComponent,
-    FooterContentComponent
+    FooterContentComponent,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prop-gms';
+  options = [
+    {path: '', redirectTo: 'login'},
+  ]
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['en', 'es']);
     this.translate.setDefaultLang('en');

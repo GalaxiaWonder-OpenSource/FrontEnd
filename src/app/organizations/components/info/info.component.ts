ca67c8144e958fb7d7d378deed2e175294d8165a
Henry Reaño <henry29.2004@hotmail.com>
2025-05-10 23:27:06 -0500

    feat(ep16): add component layout in organization context

---
+++ src/app/organizations/components/info/info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  imports: [],
+  standalone: true,
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+
+}

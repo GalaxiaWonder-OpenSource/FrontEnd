32d2d57659477cc7a0fded7e7394ca3ea34ce3a9
iconicmiau <u202224135@gmail.com>
2025-06-12 23:09:48 -0500

    feat: add members and invitations logic

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   propgmsApiBaseUrl: 'http://localhost:3000/api/v1',
   authenticationUser: 'user',
-  authenticationPassword: 'ac336883-211a-4eea-a7e8-97228a00d1de'
+  authenticationPassword: 'f0258ef5-a978-4113-bc0a-a89ccff5c71c'
 };
